46a851fbc3163fdaf911fc70585aee76aa900622
dico1631 <dicoqnfkrzl@gmail.com>
2023-05-25 00:21:19 +0900

    7-4강 실습 진행

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,13 @@
-// import { SyntheticEvent } from 'react';
 import { Link, NavLink, useNavigate } from 'react-router-dom';
 
 export default function Header() {
-  // const handleClick = (event: SyntheticEvent) => {
-  //   event.preventDefault();
-  //   const state = {};
-  //   const title = '';
-  //   const url = '/about';
-  //   window.history.pushState(state, title, url);
-  // };
+  const handleClick = (event) => {
+    event.preventDefault();
+    const state = {};
+    const title = '';
+    const url = '/about';
+    window.history.pushState(state, title, url);
+  };
 
   const navigate = useNavigate();
 
@@ -20,12 +19,29 @@ export default function Header() {
     <header>
       <nav>
         <ul>
+          {/* Web APIs history 사용 */}
+          <li>
+            <a href="/" onClick={handleClick}>home</a>
+          </li>
+          <li>
+            <a href="/about" onClick={handleClick}>about</a>
+          </li>
+          <hr />
+          {/* Link와 NavLink */}
+          <li>
+            <Link to="/">home</Link>
+          </li>
+          <li>
+            <Link to="/about">about</Link>
+          </li>
           <li>
             <NavLink to="/">home</NavLink>
           </li>
           <li>
             <NavLink to="/about">about</NavLink>
           </li>
+          <hr />
+          {/* redirect를 하는 방법 */}
           <li>
             <NavLink to="/logout">logout</NavLink>
           </li>
